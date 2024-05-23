665e4e897c9b12aa9119f6f346429982125f9448
Johannes Beyer <johannes@blue-panda.de>
2024-05-23 09:46:11 +0200

    feat: use new testing tool syntax

--- e2e/cypress/e2e/cars.cy.ts
+++ e2e/cypress/e2e/cars.cy.ts
@@ -1,26 +1,41 @@
-import {useCrudListingTests} from '@antify/ui-test-utils';
+import {useCrudListingTests, type ListingConfig} from '@antify/e2e-test-utils';
 
-useCrudListingTests({
-  listingUrl: '/cockpit/cars',
-  listingApiUrl: '/api/stores/car',
-  loadFixtures: () => cy.loadFixtures(),
-  truncateEntries: () => cy.truncateCars(),
-}).runAllTests();
+const config: ListingConfig  = {
+	listingUrl: '/cockpit/cars',
+	listingApiUrl: '/api/stores/car',
+	loadFixtures: () => cy.loadFixtures(),
+	truncateEntries: () => cy.truncateCars(),
+	tokenCookieKey: 'antt',
+	validJwtWithCrudPermissions: 'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IiIsImlzU3VwZXJBZG1pbiI6dHJ1ZSwicHJvdmlkZXJzIjpbXSwiZXhwIjozMjUwMzY3NjQwMCwiaWF0IjoxNzEwOTI0MzI2fQ.PMDZuixfvXwUfzCzVC3YXzefFi5F_SY191T7MmOFuBk',
+	validJwtWithoutCrudPermissions: 'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IiIsImlzU3VwZXJBZG1pbiI6ZmFsc2UsInByb3ZpZGVycyI6W10sImV4cCI6MzI1MDM2NzY0MDAsImlhdCI6MTcxMDkyNTA4NX0.GJh20YBmT_CYsPEXONL_1LLlHjCI5Ue5mVJJXi4VXEw',
+}
 
-describe('Feature: Users can read all cars', () => {
-  it('Scenario: On a filled database, the user should see list of cars', () => {
-    cy.log('Given there are more than 100 entries in the database');
-    cy.loadFixtures();
-
-    cy.log('When I visit the listing page');
-    cy.visit('/cockpit/cars');
-
-    cy.log('Then I should see the table with manufacturer, model, type, color as table header');
-    expect(cy.get('[data-e2e=table] th').contains('Manufacturer'));
-    expect(cy.get('[data-e2e=table] th').contains('Model'));
-    expect(cy.get('[data-e2e=table] th').contains('Type'));
-    expect(cy.get('[data-e2e=table] th').contains('Color'));
-  });
-});
-
-
+useCrudListingTests(config ,
+	{
+		loadAndShowTheListingPage: {
+			scenarioInitialLoadTheListingPage: {
+				whenDataAreLoaded: () => {
+					cy.log('Then I should see the table with manufacturer, model, type and color as table header');
+					expect(cy.get('[data-e2e=table] th').contains('Manufacturer'));
+					expect(cy.get('[data-e2e=table] th').contains('Model'));
+					expect(cy.get('[data-e2e=table] th').contains('Type'));
+					expect(cy.get('[data-e2e=table] th').contains('Color'));
+					expect(cy.get('[data-e2e=table] th').should('have.length', 5));
+				}
+			}
+		},
+		filteringTheTable: {
+			customFilterScenarios: () => {
+				it('Scenario: On a filled database, the user filter by color and get some results', () => {
+					// testCustomTableFilterScenario({
+					// 	filterAction: () => {
+					// 		cy.log('When I change the color filter to "red"');
+					// 		// TODO:: Test Color filter
+					// 		cy.get('[data-e2e=crud-table-filter] [data-e2e=dropdown]').click();
+					// 		cy.get('[data-e2e=crud-table-filter] [data-e2e=dropdown]').click();
+					// 	}
+					// }, config)
+				})
+			}
+		}
+	}).runAllTests();
