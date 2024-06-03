480a1972a12826970b25caf02d58995a0dc3135b
Johannes Beyer <johannes@blue-panda.de>
2024-06-03 14:07:37 +0200

    chore(ci): add cypress workflow

--- e2e/cypress/support/commands.ts
+++ e2e/cypress/support/commands.ts
@@ -40,6 +40,7 @@ Cypress.Commands.add('loadFixtures', () => {
   cy.exec('cd ../ && pnpm db load-fixtures core', {timeout: 10000, log: true})
     .its('stderr')
     .should('eq', '');
+	cy.exec('cd ../ && ls -la').log('HERE')
 });
 
 Cypress.Commands.add('truncateCars', () => {
